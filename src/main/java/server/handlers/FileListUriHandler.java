734aba81e2fa9df6953a36282a705b0bcc932b2e
Silakov Igor <kaluganin514@gmail.com>
2018-04-29 14:17:33 +0300

    File list handler fixes

--- src/main/java/server/handlers/FileListUriHandler.java
+++ src/main/java/server/handlers/FileListUriHandler.java
@@ -1,6 +1,5 @@
 package server.handlers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.netty.handler.codec.http.HttpRequest;
 import org.apache.commons.codec.binary.Hex;
@@ -10,48 +9,58 @@ import server.UriHandlerBased;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Получение списка файлов
+ */
 @Mapped(uri = "/list")
 public class FileListUriHandler extends UriHandlerBased {
- 
+
+    /** Текущая директория */
+    private static File currentDir = null;
+    /** Маппер, для сериализации */
+    private static final ObjectMapper mapper = new ObjectMapper();
+
     @Override
     public void process(HttpRequest request, StringBuilder buff) {
-        String dir = MainWindow.PROPS.getProperty("init_dir");
-        File file = new File(dir);
-        List<String> result = new ArrayList<>();
-        if (file.isDirectory()) {
-            File[] originalList = file.listFiles();
-            if (originalList != null) {
-                for (File item : originalList) {
-                    if (!item.isHidden()) {
-                        Map map = new HashMap<String, String>(){{
-                            put("name", item.getName());
-                            put("isDirectory", String.valueOf(item.isDirectory()));
-                        }};
-                        ObjectMapper mapper = new ObjectMapper();
-                        try {
-                            String jsonResult = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(map);
-                            result.add(jsonResult);
-                        } catch (JsonProcessingException e) {
-                            e.printStackTrace();
+        try {
+            if (request.headers().contains("path")) {
+                String decoded = new String(Hex.decodeHex(request.headers().get("path")));
+                currentDir = new File(decoded);
+            } else if (request.headers().contains("parent") && currentDir != null) {
+                currentDir = currentDir.getParentFile();
+            } else {
+                currentDir = new File(MainWindow.PROPS.getProperty("init_dir"));
+            }
+            List<String> result = new ArrayList<>();
+            if (currentDir != null && currentDir.isDirectory()) {
+                File[] originalList = currentDir.listFiles();
+                if (originalList != null) {
+                    for (File item : originalList) {
+                        if (!item.isHidden()) {
+                            Map map = new HashMap<String, String>(){{
+                                put("name", item.getName());
+                                put("isDirectory", String.valueOf(item.isDirectory()));
+                                put("path", item.getAbsolutePath());
+                            }};
+                            result.add(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(map));
                         }
                     }
                 }
-                try {
-                    ByteArrayOutputStream out = new ByteArrayOutputStream();
-                    new ObjectOutputStream(out).writeObject(result.toArray());
-                    String serialized = new String(Hex.encodeHex(out.toByteArray()));
-                    buff.append(serialized);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                new ObjectOutputStream(out).writeObject(result.toArray());
+                String serialized = new String(Hex.encodeHex(out.toByteArray()));
+                buff.append(serialized);
+            } else {
+                throw new IllegalStateException("Путь " + currentDir + " не является директорией или путь null.");
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
