269c02c40f8ed24fa8388f446e740a4752f66346
Silakov Igor <kaluganin514@gmail.com>
2018-04-29 09:54:26 +0300

    File list -> to List<serialize(Map)> -> to client

--- src/main/java/server/handlers/FileListUriHandler.java
+++ src/main/java/server/handlers/FileListUriHandler.java
@@ -1,13 +1,21 @@
 package server.handlers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.netty.handler.codec.http.HttpRequest;
+import org.apache.commons.codec.binary.Hex;
 import ui.MainWindow;
 import server.Mapped;
 import server.UriHandlerBased;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Mapped(uri = "/list")
 public class FileListUriHandler extends UriHandlerBased {
@@ -22,10 +30,27 @@ public class FileListUriHandler extends UriHandlerBased {
             if (originalList != null) {
                 for (File item : originalList) {
                     if (!item.isHidden()) {
-                        result.add(item.getAbsolutePath());
+                        Map map = new HashMap<String, String>(){{
+                            put("name", item.getName());
+                            put("isDirectory", String.valueOf(item.isDirectory()));
+                        }};
+                        ObjectMapper mapper = new ObjectMapper();
+                        try {
+                            String jsonResult = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(map);
+                            result.add(jsonResult);
+                        } catch (JsonProcessingException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
-                buff.append(result.toString());
+                try {
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    new ObjectOutputStream(out).writeObject(result.toArray());
+                    String serialized = new String(Hex.encodeHex(out.toByteArray()));
+                    buff.append(serialized);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
